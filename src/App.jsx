e214b411d92f4dea33ad5e8600f41dcb839cdc95
Murilo Missura <murilo@Mac-mini-de-Murilo.local>
2024-08-26 15:10:47 -0300

    refactor: improvements main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Logo from './assets/images/logo.svg';
 import { BsWhatsapp } from 'react-icons/bs';
 import { GiCheckMark } from 'react-icons/gi';
-// import { SiGoogleads } from 'react-icons/si';
+import { SiGoogleads } from 'react-icons/si';
 import { HiOutlineDesktopComputer } from 'react-icons/hi';
-// import Image from './assets/images/ads1.png';
+import { FaCode } from "react-icons/fa";
+import Image from './assets/images/ads1.png';
 
 import { TitlePage } from './components/TitlePage';
 import { CardInline } from './components/CardInline';
@@ -19,97 +20,185 @@ export const App = () => {
             alt='Logo Missura Networks'
             title='Logo Missura Networks'
           />
-          <ul className='flex gap-3'>
-            <li>SOBRE</li>
-            <li>SERVIÇOS</li>
-            <li>CONTATO</li>
-          </ul>
+         <section className='mt-10'>
+        <a
+          href='https://wa.me/5519971027173'
+          target='_blank'
+          className='bg-green-700 text-white py-3 px-4 flex justify-center 
+          items-center gap-3 rounded-xl mb-12 cursor-pointer'
+        >
+          <BsWhatsapp /> Fale conosco
+        </a>
+      </section>
         </div>
       </nav>
 
       <div className='bg-image1 mb-12'>
         <div className='w-full bg-black bg-opacity-30 py-20'>
           <div className='w-full flex flex-col justify-center items-center px-4'>
-            <TitlePage title='A Missura Networks' />
+              <TitlePage
+                title='A Missura Networks'
+              />
 
             <CardInline>
-              <p className='text-white 2xl:text-2xl lg:text-2xl text-xl text-center font-bold'>
-                Temos como objeto ajudar as empresas a alcançarem melhores
-                resultados online através do marketing digital. <br />{' '}
-                Desenvolvemos sites profissionais e customizados, landing pages
-                e gestão de tráfego pago com anúncios no Google. <br /> Criamos
-                estratégias personalizadas, conseguimos ajudar sua empresa a
-                decolar.
+              <p className='text-white 2xl:text-2xl lg:text-2xl text-xl 2xl:text-start 
+              xl:text-start lg:text-start text-center font-bold mb-10'>
+              Temos como objeto ajudar as empresas a alcançarem melhores
+                resultados online através do marketing digital.
               </p>
+
+            <ul className='grid 2xl:grid-cols-2 xl:grid-cols-2 grid-cols-1 
+              justify-between items-center gap-10 m-auto h-full'>
+
+              <li className='text-white 2xl:text-2xl lg:text-2xl text-xl 2xl:text-start 
+              xl:text-start lg:text-start text-center font-light'>
+                
+                Desenvolvemos sites profissionais e customizados, landing pages
+                e gestão de tráfego patrocinado com anúncios no Google.
+                   
+              </li>
+              <li className='text-white 2xl:text-2xl lg:text-2xl text-xl 2xl:text-start 
+              xl:text-start lg:text-start text-center font-light'>
+              Planejamos e criamos estratégias personalizadas para cada tipo de negócio, ajudamos sua empresa a
+                decolar na internet.<br />
+                
+              </li>
+              </ul>
+
             </CardInline>
           </div>
         </div>
       </div>
 
-      <div className='w-full  bg-gradient-to-r from-cyan-600 bg-[#2E72A4] h-96'>
-        <div className='m-auto h-full 2xl:w-10/12 xl:w-10/12 px-4 flex items-center justify-between'>
-          <CardInline>
-            <h1 className='text-3xl text-yellow-400 flex justify-center items-center text-center'>
-              <GiCheckMark />
-              Criação de Sites Customizados e Responsivos
-            </h1>
-            <h1 className='text-3xl text-yellow-400 flex justify-center items-center text-center'>
-              <GiCheckMark />
-              Criação de Landing Pages Profissionais e Responsivas
-            </h1>
-            <h1 className='text-3xl text-yellow-400 flex justify-center items-center text-center'>
-              <GiCheckMark />
-              Gestão de Tráfego Com Anúncios no Google
-            </h1>
-          </CardInline>
-        </div>
-      </div>
-
       <main className='m-auto w-full my-12'>
-        <div className='bg-image'>
-          <div className='w-full bg-black bg-opacity-30 py-14'>
+
+      <div className='bg-image2 mb-12'>
+          <div className='w-full bg-black bg-opacity-70 py-20'>
             <div className='w-full flex flex-col justify-center items-center px-4'>
               <TitlePage
                 title='Criação de sites'
+                icon={<FaCode />}
+              />
+
+            <CardInline>
+
+            <p className='text-white 2xl:text-2xl lg:text-2xl text-xl 2xl:text-start 
+              xl:text-start lg:text-start text-center font-bold mb-10'>
+              Sites profissionais responsivos e customizados
+                  conforme a necessidade de cada cliente.
+              </p>
+                          
+              <ul className='grid 2xl:grid-cols-2 xl:grid-cols-2 grid-cols-1 
+              justify-between items-center gap-10 m-auto h-full'>
+
+              <li className='text-white 2xl:text-2xl lg:text-2xl text-xl 2xl:text-start 
+              xl:text-start lg:text-start text-center font-light'>
+                   Aumente a visibilidade de sua empresa 
+                   com um site personalizado e destaque-se no mercado.<br />
+                   
+              </li>
+              <li className='text-white 2xl:text-2xl lg:text-2xl text-xl 2xl:text-start 
+              xl:text-start lg:text-start text-center font-light'>
+              Ajudamos sua marca a alcançar o sucesso desejado através de páginas
+                  personalizadas e eficientes. 
+              </li>
+              </ul>
+              
+            </CardInline>
+          </div>
+        </div>
+      </div>
+
+
+        <div className='bg-image3 mb-12'>
+          <div className='w-full bg-black bg-opacity-30 py-20'>
+            <div className='w-full flex flex-col justify-center items-center px-4'>
+              <TitlePage
+                title='Landing pages'
                 icon={<HiOutlineDesktopComputer />}
               />
 
               <CardInline>
-                <p className='text-white 2xl:text-2xl lg:text-2xl text-xl text-center font-bold'>
-                  Sites profissionais e Landing Pages responsivas e customizadas
-                  conforme a necessidade de cada cliente. <br /> Ajudamos sua
-                  marca a alcançar o sucesso desejado através de páginas
-                  personalizadas e eficientes.
-                </p>
+
+              <ul className='grid 2xl:grid-cols-2 xl:grid-cols-2 grid-cols-1 
+              justify-between items-center gap-10 m-auto h-full px-4'>
+
+              <li className='text-white 2xl:text-2xl lg:text-2xl text-xl 2xl:text-start 
+              xl:text-start lg:text-start text-center font-light'>
+                 Landing Pages responsivas e customizadas
+                  conforme a necessidade de cada cliente. <br /> 
+                   
+              </li>
+              <li className='text-white 2xl:text-2xl lg:text-2xl text-xl 2xl:text-start 
+              xl:text-start lg:text-start text-center font-light'>
+              Aumente a taxa de conversão de visitantes em clientes com uma landing page bem projetada.<br />
+              </li>
+              </ul>
+
               </CardInline>
             </div>
           </div>
         </div>
+
+
+        <div className='bg-image mb-12'>
+          <div className='w-full bg-black bg-opacity-30 py-20'>
+            <div className='w-full flex flex-col justify-center items-center px-4'>
+              <TitlePage
+                title='Gestão de tráfego'
+                icon={<SiGoogleads />}
+              />
+
+              <CardInline>
+
+              <ul className='grid 2xl:grid-cols-2 xl:grid-cols-2 grid-cols-1 
+              justify-between items-center gap-10 m-auto h-full px-4'>
+
+              <li className='text-white 2xl:text-2xl lg:text-2xl text-xl 2xl:text-start 
+              xl:text-start lg:text-start text-center font-light'>
+                  Gestão de tráfego com anúncios patrocinados no Google. <br />
+                  Exiba seus anúncios para as pessoas certas, no lugar certo e na hora certa.<br />
+                  Faça a diferença e saia na frente da concorrência.
+                   
+              </li>
+              <li className='text-white 2xl:text-2xl lg:text-2xl text-xl 2xl:text-start 
+              xl:text-start lg:text-start text-center font-light'>
+              No Brasil, 93% da população utilizam o Google para fazer buscas por serviços ou produtos.<br />
+                  Ajudamos a aumentar suas vendas com campanhas bem estruturadas e completas.
+              </li>
+              </ul>
+
+              </CardInline>
+            </div>
+          </div>
+        </div>
+
+
       </main>
 
       <CardInline>
         <h1
           className='text-xl text-center 2xl:text-3xl xl:text-3xl lg:text-3xl 
-           my-10 mx-auto font-bold'
+           my-10 mx-4 font-bold'
         >
-          Quer divulgar sua empresa ou produto e atrair mais clientes?<br></br>
-          Entre em contato e solicite um orçamento
+          Quer divulgar sua empresa ou produto e atrair mais clientes?<br />
+          Entre em contato e solicite um orçamento.
         </h1>
       </CardInline>
 
-      <section className='flex justify-center items-center' id='contact'>
+      <section className='flex justify-center items-center'>
         <a
           href='https://wa.me/5519971027173'
           target='_blank'
-          className='bg-green-700 text-white py-3 px-10 flex justify-center items-center gap-3 rounded-xl mb-12'
+          className='bg-green-700 text-white py-3 px-10 flex justify-center items-center gap-3 rounded-xl mb-12 cursor-pointer'
         >
           <BsWhatsapp /> Fale conosco
         </a>
       </section>
 
       <footer className='text-center bg-gradient-to-r from-cyan-600 bg-[#2E72A4] text-sm'>
-        <p className='mt-20 text-white p-8'>
-          &copy; Copyright 2024 by Missura Networks
+        <p className='mt-6 text-white p-8'>
+          &copy; Copyright 2024 Missura Networks
           <br />
           CNPJ: 53.447.302/0001-79
         </p>
--- src/components/CardInline/index.tsx
+++ src/components/CardInline/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const CardInline = ({ children }) => {
   return (
-    <div className='2xl:w-1/2 xl:w-1/2 w-11/12 mx-auto mt-6'>{children}</div>
+    <div className='2xl:w-full xl:w-2/3 w-full mx-auto mt-6'>{children}</div>
   );
 };
